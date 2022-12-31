742d18dfbecbbff843e8603257addd00e217af75
Victor Hugo B <victorhugobeppler@gmail.com>
2022-12-31 17:17:35 -0300

    build(Criado rotas e adicionado dependencias)
    foram criadas as seguintes rotas:
    
    /pools que é utilizada para criar o bolões,
    /guesses/count contagem dos palpites,
    /users/count contagem dos usuários.
    
    Dependencias adicionadas:
    
    - short-unique-id //utilizada para criar IDs unicos.
    - zod

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
+import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
   log: ["query"],
@@ -20,7 +22,37 @@ async function bootstrap() {
     return { count };
   });
 
-  await fastify.listen({ port: 3000 , host: "0.0.0.0"});
+  fastify.post("/pools", async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+
+    const { title } = createPoolBody.parse(request.body);
+
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    return reply.status(201).send({ code });
+  });
+
+  fastify.get("/guesses/count", async () => {
+    const count = await prisma.guess.count();
+    return { count };
+  });
+
+  fastify.get("/users/count", async () => {
+    const count = await prisma.user.count();
+    return { count };
+  });
+
+  await fastify.listen({ port: 3005 });
 }
+//, host: "0.0.0.0"
 
 bootstrap();
